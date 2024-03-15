dacff8bd3d92397b782ce7f47d996cd0a0724335
ayathree <nobanitaayathree333@gmail.com>
2024-03-16 01:39:11 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { FaSearch } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     
+      <nav>
+      <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <h1 className=" text-3xl font-bold ">Recipe Calories</h1>
+  </div>
+  <div className="navbar-center hidden lg:flex  gap-5">
+    <a className="font-semibold">Home</a>
+    <a className="font-semibold">Recipe</a>
+    <a className="font-semibold">About</a>
+    <a className="font-semibold">Search</a>
+  </div>
+  <div className="navbar-end flex-none gap-2 ">
+    <div className="form-control relative hidden lg:block">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto p-7 " />
+      <FaSearch className="absolute top-3 m-3" />
+    </div>
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle bg-green-400 ">
+        <div className="w-10 rounded-full">
+        <FaRegUserCircle className="text-4xl"/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 block lg:hidden ">
+        <li><a> Home</a></li>
+        <li><a>Recipe</a></li>
+        <li><a>About</a></li>
+        <li><a>Search</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+      </nav>
+      
+     
     </>
   )
 }
